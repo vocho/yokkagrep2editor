5942bb22fd0469a068304792203468b77b687d76
vocho <pongar@gmail.com>
2015-06-09 23:28:41 +0900

    Supports the old version of YokkaGrep

--- NoEditor.cs
+++ NoEditor.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
+using System.Linq; // Any() => Count > 0
 
 class dummyexe
 {
@@ -13,15 +14,17 @@ class dummyexe
         {
             Environment.Exit(0);
         }
+        //System.Windows.Forms.MessageBox.Show(args[0]);
 
         Queue<string> param = new Queue<string> (args[0].Trim('"').Split(new [] {"->\\<-"}, StringSplitOptions.None));
-        string text_path = param.Dequeue().Replace("/G=", "");
-        string keyword = param.Dequeue();
-        param.Dequeue();
-        param.Dequeue();
-        string line_number = param.Dequeue();
-        param.Dequeue();
-        string column_number = param.Dequeue();
+        //param.ToList().ForEach(s => System.Windows.Forms.MessageBox.Show(s.ToString()));
+        string text_path        = param.Any() ? param.Dequeue().Replace("/G=", "") : "";
+        string keyword          = param.Any() ? param.Dequeue() : "";
+        string dummy1           = param.Any() ? param.Dequeue() : "";
+        string dummy2           = param.Any() ? param.Dequeue() : "";
+        string line_number      = param.Any() ? param.Dequeue() : "";
+        string dummy3           = param.Any() ? param.Dequeue() : "";
+        string column_number    = param.Any() ? param.Dequeue() : "";
 
         Process process = new Process();
         process.StartInfo.FileName = "\"C:\\Program Files (x86)\\sakura\\sakura.exe\"";
